db797d650ea2b5689e98cc07c6480865bd041034
ToothpasteVixen <55934692+RoyaleNoir@users.noreply.github.com>
2022-03-17 20:20:58 +1300

    Update _builddynamic.bat

--- stdshaders/test_project/_builddynamic.bat
+++ stdshaders/test_project/_builddynamic.bat
@@ -1,7 +1,8 @@
 @echo off
 set project_dir="%cd%"
 cd ..
-python buildshaders.py -dynamic -shaders "%project_dir%\shader_lists\sm2x_dx90" -game "..\..\..\game\mod_episodic" -source "..\.." -bin_dir "D:\\SteamLibrary\\SteamApps\\common\\Source SDK Base 2013 Singleplayer\\bin\\"
+rem dx9_90 is broken
+rem python buildshaders.py -dynamic -shaders "%project_dir%\shader_lists\sm2x_dx90" -game "..\..\..\game\mod_episodic" -source "..\.." -bin_dir "D:\\SteamLibrary\\SteamApps\\common\\Source SDK Base 2013 Singleplayer\\bin\\"
 python buildshaders.py -dynamic -shaders "%project_dir%\shader_lists\sm2x_dx93" -game "..\..\..\game\mod_episodic" -source "..\.." -bin_dir "D:\\SteamLibrary\\SteamApps\\common\\Source SDK Base 2013 Singleplayer\\bin\\" -dx9_30
 python buildshaders.py -dynamic -shaders "%project_dir%\shader_lists\sm30" -game "..\..\..\game\mod_episodic" -source "..\.." -bin_dir "D:\\SteamLibrary\\SteamApps\\common\\Source SDK Base 2013 Singleplayer\\bin\\" -dx9_30 -force30
 pause
